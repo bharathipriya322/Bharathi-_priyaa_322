1d95bae69435f41284288b555fb10d88ea389f85
bharathipriya322 <bharathipriyabhuvaneswari@gmail.com>
2025-05-22 11:41:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function addToCart(productName) {
+    alert(productName + " has been added to your cart!");
+}
+
+function validateForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if (!name || !email || !message) {
+        alert("Please fill out all fields.");
+        return false;
+    }
+    alert("Message sent successfully!");
+    return true;
+}
